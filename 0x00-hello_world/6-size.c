94de2de3a181f0ce13723f2b7ac099177c2003a0
Dithsy <92625525+Dithsy@users.noreply.github.com>
2022-03-10 23:28:41 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-*main - prints the size of varios types
-*Return: 0 if exited properly, non-zero otherwise
-*/
+**main - prints the size of varios types
+**Return: 0 if exited properly, non-zero otherwise
+**/
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
