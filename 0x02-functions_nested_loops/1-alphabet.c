3c5dc8dd77735626e0d8a6199da8ec0096f161ec
Dithsy <edithagoroh@gmail.com>
2022-03-15 22:41:24 +0300

    Main types

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet -prints out alphabet
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+_putchar(x);
+
+_putchar('\n');
+}
