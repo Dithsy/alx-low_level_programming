499ebcbfe8336d64835b4736c636d5079089a402
Dithsy <edithagoroh@gmail.com>
2022-03-17 14:11:57 +0300

    Supper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* _isupper - it checks for uppercase character
+* @c: Variable text
+* Return: 1 if c is uppercase, otherwise 0.
+*/
+int _isupper(int c)
+{
+
+
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+{
+	return (0);
+}
+
+}
