ab9d966b632ec2f0f95667ba06fdf5ecc451a189
Dithsy <92625525+Dithsy@users.noreply.github.com>
2022-04-01 19:25:27 +0300

    Update 4-isalpha.c

--- 0x09-static_libraries/4-isalpha.c
+++ 0x09-static_libraries/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *  * _isalpha - check for lower case letter
- *   * @c : chiracter to check
- *    * Return:0 or 1
- *     */
+ * _isalpha - check for lower case letter
+ * @c : character to check
+ * Return:0 or 1
+ */
 
 int _isalpha(int c)
 {
-		return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
+	return ((c >= 97 && c <= 122) || (c >= 65 && c <= 90));
 
 }
