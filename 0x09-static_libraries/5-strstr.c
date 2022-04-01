d16dc0a47acfae497539811354931719750c74d1
Dithsy <92625525+Dithsy@users.noreply.github.com>
2022-04-01 19:27:17 +0300

    Update 5-strstr.c

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,30 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  * *_strstr - description
- *   * @haystack: string
- *    * @needle: pointer
- *     * Return: pointer
- *      */
+ * *_strstr - description
+ * @haystack: string
+ * @needle: pointer
+ * Return: pointer
+ */
 
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; haystack[i] > '\0'; i++)
+for (i = 0; haystack[i] > '\0'; i++)
+{
+	for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
 	{
-			for (j = i; haystack[j] > '\0' && needle[j - i] > '\0'; j++)
-					{
-								if (haystack[j] != needle[j - i])
-											{
-															break;
-																	}
-									}
-				if (needle[j - i] == '\0')
-						{
-									return (haystack + i);
-										}
+		if (haystack[j] != needle[j - i])
+		{
+			break;
+		}
 	}
-	return (0);
+	if (needle[j - i] == '\0')
+	{
+		return (haystack + i);
+	}
+}
+return (0);
 }
