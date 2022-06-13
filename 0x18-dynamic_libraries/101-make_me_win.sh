e2e30ca45e45ccfa103cc551a9743e4eacbeadde
Eddith A <92625525+Dithsy@users.noreply.github.com>
2022-06-14 00:12:03 +0300

    ww

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Dithsy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmask.so
+wget -P /tmp https://github.com/Dithsy/alx-low_level_programming/master/0x18-dynamic_libraries/libmask.so
 export LD_PRELOAD=/tmp/libmask.so
